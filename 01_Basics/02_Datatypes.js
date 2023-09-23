cd68d7e3609e13e7f9b2044d80583cf2cc324f7f
Ayush Singh <145449339+ayushsingh-ayushsingh@users.noreply.github.com>
2023-09-23 10:27:54 +0000

    Completed

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+//Always focus on code readability
+console.log("node 01_Basics/02_Datatypes.js");
+
+"use strict";//Treats all JS code as newer version
+
+//alert( 3 + 3 ) // We are using Node.js not Browser
+
+console.log("Sum of 3 and 4: ")
+console.log( 3 + 4 )
+
+let name = "Ayush Singh"
+let age = 19
+let isLoggedIn = false
+let state = null;
+let city;
+
+/*  Some of the Data-types are listed below
+number
+bigint (for very large numbers)
+string (Double inverted commas)
+boolean
+null (stand-alone value)
+undefined
+symbol (unique)
+object  */
+
+console.log(typeof "Ayush Singh") //string
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
