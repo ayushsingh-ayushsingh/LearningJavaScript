e55e525eb1cda5db0239bca11256df1717a299e1
Ayush Singh <145449339+ayushsingh-ayushsingh@users.noreply.github.com>
2023-09-25 10:54:38 +0000

    Completed

---
+++ 01_Basics/03_operations.js
@@ -0,0 +1,12 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+//some arithmetic operations...
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3/2)
+console.log(3%2)
+//Power
+console.log(3**2)
