3128ac73ff1283d6a00f736e9d7c665cfbf6b92c
Ayush Singh <145449339+ayushsingh-ayushsingh@users.noreply.github.com>
2023-09-25 08:53:55 +0000

    Completed

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,29 @@
+//Typecasting in JS
+let score = 33
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof(score))
+
+//"33"=>33
+//"33abc"=>NaN
+// true => 1
+//false => 0
+// undefined => NaN
+//Null => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//For boolean...
+//1=>true; 0=>false
+//""=> fasle
+//"string"=> true
+
+let number = 33
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
