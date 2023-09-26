5071513b4044a516456ca6e7ea330d41083667a5
Ayush Singh <145449339+ayushsingh-ayushsingh@users.noreply.github.com>
2023-09-26 15:08:40 +0000

    Completed

---
+++ LearningJavaScript/index.js
@@ -0,0 +1,6 @@
+//JavaScript console API
+console.log("Hello World");
+//alert("This is for Warning Pop-Up")
+console.warn("This is a warning...")
+console.log("How are you feeling today???")
+console.error("This is an error");
