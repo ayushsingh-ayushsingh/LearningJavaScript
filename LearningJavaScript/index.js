0f052d5905b19cf928b99e2dc4c0f42bf065f23b
Ayush Singh <145449339+ayushsingh-ayushsingh@users.noreply.github.com>
2023-09-26 18:26:14 +0000

    Complete

--- LearningJavaScript/index.js
+++ LearningJavaScript/index.js
@@ -1,6 +1,35 @@
-//JavaScript console API
-console.log("Hello World");
-//alert("This is for Warning Pop-Up")
-console.warn("This is a warning...")
-console.log("How are you feeling today???")
-console.error("This is an error");
+// 1. Ways to print in JavaScript
+// console.log("Hello wrorl")
+// alert("Alert message pop-up")
+// document.write("This is a document write")
+
+// 2. JavaScript console API
+// console.warn("This is a warning...")
+// console.log("How are you feeling today???")
+// console.error("This is an error");
+
+//JavaScript variables
+//Containers to store data values
+
+//4. Here are the Data-types
+//Numbers
+var number1 = 34;
+var number2 = 56.769;
+console.log(number1 + number2);
+
+//Strings
+var str1 = "This is a string"
+var str2 = ' and This is another string'
+console.log(str1 + str2)
+
+//Objects in JavaScript
+var marks = {
+    person1: 78,
+    person2: 70,
+    person3: 29.19997,
+    person4: 64,
+    person5: 88
+}
+//Printing the marks
+console.log(marks.person3)
+console.log(marks.person5)
